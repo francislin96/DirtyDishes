c1de4bc3953e3728aee5c5b883c52c8ca81bed4d
francislin96 <francislin96@yahoo.com>
2018-01-13 14:37:50 -0800

    Merged dirty_dish_bandits.py and LoadCell.py
    Merged dirty_dish_bandits.py and LoadCell.py
    Fixed bugs in change_array
    Created a main function

--- dirty_dish_bandits.py
+++ dirty_dish_bandits.py
@@ -1,33 +1,26 @@
 ### Import necessary libraries
 import numpy as np
-
-
-### Initialize at t = 0
-num_people = 100									# Initialize number of people
-weight_array = np.zeros(num_people)					# Store weights in an array
-weight_before = measure_sink()						# Initial weight in sink
-faces = load_faces(num_people)						# Stores template faces for each person
-names = ["Brooke", "Eric", "Francis", "Nithin"]		# Stores names of each person
-
-
-### Iterate for t > 0
-while True:
-	if person_is_detected():
-		person_index = identify_person()											# Find the index of weight_array that belongs to person detected
-		weight_after = measure_sink()												# Takes measurement from load cell
-		delta_weight = weight_after - weight_before									# Calculate the change in weight
-		weight_array = change_array(person_index, weight_array, delta_weight)		# Update the weight array accordingly
-		weight_before = weight_after												# Update weight_before for next iteration
-
-	display_results(faces, num_people, weight_array)								# Continuously display weight_array
-
-
-
+import RPi.GPIO as GPIO
+import time
+import sys
+from hx711 import HX711
 
 ### Helper functions
 
 def measure_sink():
 	# Returns a double corresponding with how much weight is in the sink
+    try:
+        val = hx.get_weight(5)
+        #print(val)
+        hx.power_down()
+        hx.power_up()
+        time.sleep(.01)
+    except (KeyboardInterrupt, SystemExit):
+        GPIO.cleanup()
+        print("Exiting..")
+        sys.exit()
+    return val
+
 
 def load_faces(num_people):
 	# Returns an array of faces as templates for future face detection
@@ -37,11 +30,6 @@ def person_is_detected():
 
 def identify_person():
 	# Returns an int corresponding with that person's index in weight_array
-
-def display_results(faces, weight_array):
-	# Updates the continuous display with the weight of each person
-	for i in names:
-		print "%s: %dg\n" % (name[i], weight_array[i])
 
 def change_array(person_index, weight_array, delta_weight,):
 	# Updates weight_array based on how the sink changed
@@ -54,11 +42,47 @@ def change_array(person_index, weight_array, delta_weight,):
 	else:
 		# find how much each person contributes to the sink, and redistribute based on ratio
 		sum_array = sum(weight_array) - weight_array[person_index]
-		for i in range(0, len(weight_array)-1):
-			if i == person_index:
-				weight_array[i] = 0
-			else:
+		for i in range(0, len(weight_array)):
+			if i != person_index:
 				weight_array[i] = weight_array[i] + float(weight_array[i])/sum_array * (weight_array[person_index] + delta_weight)
+		weight_array[person_index] = 0
 	# return new weight_array
 	return weight_array
 
+def display_results(faces, weight_array):
+	# Updates the continuous display with the weight of each person
+	for i in names:
+		print "%s: %dg\n" % (name[i], weight_array[i])
+
+
+
+
+### MAIN FUNCTION
+def main():
+	# Initialize parameters at t = 0
+	num_people = 100									# Initialize number of people
+	weight_array = np.zeros(num_people)					# Store weights in an array
+	weight_before = measure_sink()						# Initial weight in sink
+	faces = load_faces(num_people)						# Stores template faces for each person
+	names = ["Brooke", "Eric", "Francis", "Nithin"]		# Stores names of each person
+
+	# Initialize load cell
+	hx = HX711(5, 6)
+	hx.set_reading_format("LSB", "MSB")
+	hx.set_reference_unit(92)       # Calibrate reference unit to 1g
+	hx.reset()
+	hx.tare()
+
+	# Iterate for t > 0
+	while True:
+		if person_is_detected():
+			person_index = identify_person()											# Find the index of weight_array that belongs to person detected
+			weight_after = measure_sink()												# Takes measurement from load cell
+			delta_weight = weight_after - weight_before									# Calculate the change in weight
+			weight_array = change_array(person_index, weight_array, delta_weight)		# Update the weight array accordingly
+			weight_before = weight_after												# Update weight_before for next iteration
+	display_results(faces, num_people, weight_array)									# Continuously display weight_array
+
+
+if __name__ == "__main__":
+	main()
